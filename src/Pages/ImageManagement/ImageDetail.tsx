d5852f92aec31c05d0233b634dced57898a5f880
khoadv42 <kematthangbang@gmail.com>
2023-05-25 11:51:31 +0700

    add intersection observer image

--- src/Pages/ImageManagement/ImageDetail.tsx
+++ src/Pages/ImageManagement/ImageDetail.tsx
@@ -1,21 +1,63 @@
 import { CopyOutlined, DeleteOutlined, EditOutlined, UploadOutlined } from "@ant-design/icons"
 import { Input, Select, Upload } from "antd"
-import { useEffect, useState } from "react";
+import { RcFile } from "antd/es/upload/interface";
+import { useEffect, useRef, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
+import { InView, useInView } from "react-intersection-observer";
 import { ImageCrop } from "../../Components/ImageCrop/ImageCrop";
 import { ButtonActionImage, ButtonEdit, ButtonUpload, ImageDetailBlock, ModalEditImage } from "./styled"
 
+const getBase64 = (file: RcFile): Promise<string> =>
+    new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result as string);
+        reader.onerror = (error) => reject(error);
+    });
+
 export const ImageDetail = () => {
     const [open, setOpen] = useState<boolean>(false);
-    const { control , watch, getValues} = useForm();
+    const { control, watch, getValues } = useForm();
+    // data example intersection obsever
+    const [items, setItems] = useState<any>([
+        { base64: 'https://s.pinimg.com/webapp/center-77497603.png' },
+        { base64: 'https://s.pinimg.com/webapp/topRight-d0230035.png' },
+        { base64: 'https://s.pinimg.com/webapp/left-511a9304.png' },
+        { base64: 'https://i.pinimg.com/236x/f1/13/df/f113df475d4566caa0075c6729960fa3.jpg' },
+        { base64: 'https://i.pinimg.com/236x/f1/13/df/f113df475d4566caa0075c6729960fa3.jpg' },
+        { base64: 'https://i.pinimg.com/236x/f4/dc/58/f4dc58f3bddf1c5b5249511820246df8.jpg' },
+        { base64: 'https://i.pinimg.com/236x/88/05/12/8805128eef83a0d8b724567611ddf7a1.jpg' },
+        { base64: 'https://s.pinimg.com/webapp/serve-my-drinks-263547ea.png' },
+        { base64: 'https://s.pinimg.com/webapp/shop-bd0c8a04.png' },
+        { base64: 'https://s.pinimg.com/webapp/deck-of-dreams-fb527bf1.png' },
+    ]);
+
+    const { ref: intersectionObserverRef, inView, entry } = useInView({
+        threshold: 1
+    });
+
     const [currentAspect, setCurrentAspect] = useState([1, 1]);
     const watchAspect = watch("aspect");
+
     useEffect(() => {
         const [width, height] = (watchAspect + "").split(":")
         setCurrentAspect([+width, +height]);
     }, [watchAspect])
 
-    return <ImageDetailBlock>
+    const coppyToClipboard = (text: string) => {
+        navigator.clipboard.writeText(text);
+    }
+
+    const removeItem = (item: any, index: number) => {
+
+    }
+
+    const beforeUpload = async (file: any, FileList: RcFile[]) => {
+        const base64 = await getBase64(file);
+        setItems([...items, { base64 }]);
+    }
+
+    return <ImageDetailBlock ref={intersectionObserverRef}>
         <div>
             <span>Ảnh {'>'} Chi tiết thư mục</span>
         </div>
@@ -33,25 +75,34 @@ export const ImageDetail = () => {
                     <div className="actions">
                         <span>Kéo thả để tải ảnh</span>
                         <span>Hoặc</span>
-                        <Upload>
+                        <Upload
+                            fileList={[]}
+                            beforeUpload={beforeUpload}
+                        >
                             <ButtonUpload icon={<UploadOutlined />}>Tải ảnh</ButtonUpload>
                         </Upload>
                     </div>
                     {
-                        [1, 2, 3, 4, 5].map((item: any, index: number) => <div className="card" key={index}>
-                            <div className="action">
-                                <ButtonActionImage onClick={() => setOpen(true)} color={'blue'} icon={<EditOutlined />} title={"edit"}></ButtonActionImage>
-                                <ButtonActionImage color={'orange'} icon={<CopyOutlined />} title={"copy"}></ButtonActionImage>
-                                <ButtonActionImage icon={<DeleteOutlined />} title={"delete"}></ButtonActionImage>
-                            </div>
-                            <div className='imagepreview'>
-                                <img src={'/images/image-preview.svg'} alt="/images/image-preview.svg" />
-                            </div>
-                            <div className="title">
-                                Ảnh dịch vụ ABCXYZ
-                            </div>
-                            <div className="createDate">05/04/2023 2:06:00</div>
-                        </div>)
+                        items.map((item: any, index: number) =>
+                        (<InView>
+                            {({ ref, inView }) => (<div className="card" key={index} ref={ref}>
+                                <div className="action">
+                                    <ButtonActionImage onClick={() => setOpen(true)} color={'blue'} icon={<EditOutlined />} title={"edit"}></ButtonActionImage>
+                                    <ButtonActionImage onClick={() => coppyToClipboard('hello word')} color={'orange'} icon={<CopyOutlined />} title={"copy"}></ButtonActionImage>
+                                    <ButtonActionImage onClick={() => removeItem(item, index)} icon={<DeleteOutlined />} title={"delete"}></ButtonActionImage>
+                                </div>
+                                <div className='imagepreview'>
+                                    <img src={item.base64 && inView ? item.base64 : '/images/image-preview.svg'}
+                                        alt="/images/image-preview.svg"
+                                        width={75} height={75}
+                                    />
+                                </div>
+                                <div className="title">
+                                    Ảnh dịch vụ ABCXYZ
+                                </div>
+                                <div className="createDate">05/04/2023 2:06:00</div>
+                            </div>)}
+                        </InView>))
                     }
                 </div>
             </div>
@@ -105,7 +156,7 @@ export const ImageDetail = () => {
                 />
             </div>
             <div className="image-preview">
-                <ImageCrop aspect={currentAspect[0]/currentAspect[1]} />
+                <ImageCrop aspect={currentAspect[0] / currentAspect[1]} />
             </div>
         </ModalEditImage>
     </ImageDetailBlock>
--- src/Pages/ImageManagement/styled.tsx
+++ src/Pages/ImageManagement/styled.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled"
+import { borderColor } from "@mui/system"
 import { Button, Modal } from "antd"
 import { theme } from "../../theme"
 
@@ -121,6 +122,53 @@ export const ModalEditImage = styled(Modal)(() => ({
         '.danger': {
             color: 'red'
         },
+        '.ant-modal-content': {
+            padding: 0,
+            '.ant-modal-close': {
+                color: '#FFFFFF'
+            },
+            '.ant-modal-header': {
+                backgroundColor: theme.colors.primary,
+                '.ant-modal-title': {
+                    padding: '18px 29px',
+                },
+            },
+            '.ant-modal-body': {
+                padding: '24px 29px',
+            },
+            '.ant-modal-footer': {
+                padding: '24px 29px',
+                '.ant-btn-default': {
+                    width: 141,
+                    boxSizing: 'border-box',
+                    background: '#FFFFFF',
+                    fontWeight: 600,
+                    color: `${theme.colors.primary}!important`,
+                    borderColor: `${theme.colors.primary}!important`,
+                    border: '1px solid rgba(0, 0, 0, 0.4)',
+                    ':not(:disabled):hover': {
+                        color: `${theme.colors.primary}!important`,
+                        borderColor: `${theme.colors.primary}!important`
+                    }
+                },
+                '.ant-btn-primary': {
+                    fontWeight: 600,
+                    width: 141,
+                    background: theme.colors.primary,
+                    borderRadius: 5,
+                }
+            }
+        },
+        '.ant-modal-title': {
+            fontFamily: 'Open Sans',
+            fontStyle: 'normal',
+            fontWeight: 600,
+            fontsize: 14,
+            display: 'flex',
+            alignItems: 'center',
+            color: '#FFFFFF',
+            backgroundColor: theme.colors.primary,
+        },
         '.info-edit': {
             display: 'flex',
             flexWrap: 'wrap',
@@ -130,7 +178,7 @@ export const ModalEditImage = styled(Modal)(() => ({
                 width: '48%',
                 display: 'flex',
                 justifyContent: 'space-between',
-                '.ant-select':{
+                '.ant-select': {
                     width: '350px'
                 },
                 input: {
@@ -141,7 +189,7 @@ export const ModalEditImage = styled(Modal)(() => ({
         '.image-preview': {
             marginTop: 10,
             width: '100%',
-            textAlign: 'center',
+            textAlign: 'center'
         }
     },
 }))
